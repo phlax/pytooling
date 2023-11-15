1012d29a6ac984a1f970ba4755360c35417cf74b
Ryan Northey <ryan@synca.io>
2023-11-15 08:44:05 +0000

    gh-actions/

--- gh-actions/using/steps/create-steps.sh
+++ gh-actions/using/steps/create-steps.sh
@@ -3,12 +3,12 @@
 mkdir -p .tmp.action
 if [[ "$FORMAT" == "yaml" ]]; then
     __STEPS__=$(printf "%s\n" "${STEPS}" \
-        | sed 's/\([^\\]\)%{{ /\1${{ /g; s/^/  /')
+        | sed 's/\([^\\]\)${{ /\\\1${{ ; s/\([^\\]\)%{{ /\1${{ /g; s/^/  /')
     __STEPS__="
 ${__STEPS__}"
 else
     __STEPS__=$(printf "%s\n" "${STEPS}" \
-        | sed 's/\([^\\]\)%{{ /\1${{ /g')
+        | sed 's/\([^\\]\)${{ /\\\1${{ ; s/\([^\\]\)%{{ /\1${{ /g')
 fi
 export __STEPS__
 cat "$1" | envsubst > .tmp.action/action.yml
